3c8152f6781345bd0300636adf1502e195aed7c4
RohanPolkum <112601668+RohanPolkum@users.noreply.github.com>
2022-10-27 12:46:14 -0700

    Add files via upload

---
+++ Assignment-ICA8/src/main/java/Urinals.java
@@ -0,0 +1,28 @@
+/*
+Author: Rohan Goud Polkumpally
+ASURITE: rpolkump
+*/
+import java.util.*;
+public class Urinals {
+    List<String> arr=new ArrayList<String>();
+    void getString()
+    {
+
+    }
+    void openFile()
+    {
+
+    }
+    void countUrinals()
+    {
+
+    }
+    boolean goodString()
+    {
+
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
