4737449f3c0dfb5ff7751f71a150e882b392baa8
RohanPolkum <rpolkump@asu.edu>
2022-10-27 21:35:58 -0700

    Open file case failed

--- Assignment-ICA8/src/main/java/Urinals.java
+++ Assignment-ICA8/src/main/java/Urinals.java
@@ -1,4 +1,6 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
 Author: Rohan Goud Polkumpally
@@ -28,6 +30,10 @@ public class Urinals {
             else return false;
         }
         return true;
+    }
+    public ArrayList<String> openFile(String File) throws FileNotFoundException{
+        return new ArrayList<>();
+
     }
     public static void main(String[] args) {
         System.out.println("");
--- Assignment-ICA8/src/main/java/UrinalsTest.java
+++ Assignment-ICA8/src/main/java/UrinalsTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class UrinalsTest {
     @Test
     void isTestValidString1(){
@@ -17,5 +20,13 @@ public class UrinalsTest {
     void isTestValidString3(){
         Urinals u=new Urinals();
         Assertions.assertTrue(u.goodString(""));
+    }
+
+    @Test
+    void isFileOpen1() throws FileNotFoundException {
+        Urinals u=new Urinals();
+        ArrayList<String> a=u.openFile("file1");
+        Assertions.assertEquals(a.size(),1);
+
     }
 }
